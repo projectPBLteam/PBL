28f0d94b6ba0ac379b27cbd26918c52127314e92
JungBoHyun <wjdqhgus07@naver.com>
2025-05-16 23:46:48 +0900

    테이블 만들기(수정중)

--- PBL/filedown/models.py
+++ PBL/filedown/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# 데이터 모델(장고 내부에 테이블을 만듬)
+class Fileinput(models.Model):
+    filedata = models.CharField()
+    tableN = models.CharField()
