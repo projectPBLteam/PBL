771fd233f080b23b4e86bb61d2bb15b6ed62cc0e
JungBoHyun <wjdqhgus07@naver.com>
2025-05-19 03:41:08 +0900

    파일 업로드 html 추가
    post 부분 미완

--- PBL/filedown/views.py
+++ PBL/filedown/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("csv 파일을 입력받을 페이지입니다.")
+def main(request):
+    #  return HttpResponse("csv 파일을 입력받을 페이지입니다.")
+    return render(request, 'main.html')
+
+def otherpage(request):
+    # datainput = request.GET['datainput']
+    datainput = request.POST['datainput']
+    return HttpResponse("Other Page test = " + datainput)
